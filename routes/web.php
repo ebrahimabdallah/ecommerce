282844362a584c744ecc9c7d47df5fcdc49c2e15
Ebrahim Reda <119238955+ebrahimabdallah@users.noreply.github.com>
2023-08-27 20:08:36 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -42,19 +42,11 @@ Route::controller(HomeController::class)->group(function(){
 
 });
 
-
-Route::controller(ClientController::class)->group(function(){
-    
+    Route::controller(ClientController::class)->group(function(){
     Route::get('/category/{id}/{slug}','CategoryPage')->name('category');
     
     Route::get('/Product_details/{id}/{slug}','SingleProduct')->name('singleProduct');
     Route::get('/new_realse','NewRealse')->name('new_realse');
-;
-});
-
- Route::middleware(['auth','role:user'])->group(function () {
-     Route::controller(ClientController::class)->group(function(){
-    
     Route::get('/addToCart','AddToCart')->name('addToCart');
 
     Route::get('/delete-cart/{id}','DeleteCart')->name('deleteCart');
@@ -79,13 +71,9 @@ Route::controller(ClientController::class)->group(function(){
 
     Route::get('/custom_service','CustomService')->name('custom-service');
  });
-});
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -93,7 +81,6 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth','role:admin'])->group(function () {
 Route::controller(DashbordController::class)->group(function(){
 Route::get('/admin/dashboard','index')->name('admindasboard');
-
 
 });
 Route::controller(CategoryController::class)->group(function(){
